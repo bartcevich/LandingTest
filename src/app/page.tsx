87c34bd08e0f616edd032c97003d4fc097279892
bartcevich <bartcevich@tut.by>
2025-10-01 16:31:18 +0300

    added image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Header from "@/components/Header";
 import Promotion from "@/components/Promotion";
 import RegistrationForm from "@/components/RegistrationForm";
 import styles from "./home.module.css";
+import Image from "next/image";
+import sidebarImage from "@/assets/images/gold-min.png";
 
 export default function Home() {
   return (
     <main className={styles.homepage}>
       <div className={styles.registration}>
+        <div className={styles.sidebarImage}>
+          <Image src={sidebarImage} alt="logo" />
+        </div>
         <RegistrationForm />
       </div>
       <div className={styles.content}>
